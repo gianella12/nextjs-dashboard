9e00a3a842e08a66687c37008feafa19c71d62d7
gianella lastra <gianellalastra4@gmail.com>
2025-10-31 16:16:17 -0300

    nextAuth y metadatos

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,12 @@ import style from '@/app/ui/home.module.css'
 import { lusitana } from '@/app/ui/fonts';
 import AcmeLogo from '@/app/ui/acme-logo';
 import Image from 'next/image';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
+
 
 export default function Page() {
   return (
